f4b41c9fd47d35291dcb44840272119901021138
KotletCodes <215183@herschel.slough.sch.uk>
2024-05-02 10:00:34 +0100

    Add files via upload

---
+++ Govind.py
@@ -0,0 +1,41 @@
+
+import random
+words = open("WORDS.txt","r")
+word = []
+num = random.randint(0,3000)
+for x in range(0,3000):
+    word.append(words.readline())
+
+chosenword = word[num]
+print(chosenword)
+letterscorrect = 0
+count = 5
+guess = ""
+while count != 0 or letterscorrect == 5 :
+    guess = input("enter")
+    count = count -1
+    
+   
+        
+    
+    for i in range(0,len(guess)):
+        if guess[i] == chosenword[i]:
+                print("the",i+1,"letter is in the correct place")
+                letterscorrect = letterscorrect +1
+                
+        elif guess[i] in chosenword and guess[i] != chosenword[i]:
+
+                print("the",i+1,"letter is in the word but in the wrong place")
+                
+        else:
+                print("this letter",[i+1]," is not in the word")
+    print(count,"tries left")
+   
+
+        
+if count == 0:
+    print("Try again")
+else:
+    print("well done, took you",count,"tries")
+
+                   
